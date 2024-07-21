829c746a5d6b6c3f905e48e9c489a647c25cade9
Nafis Shamsevaliev <shamsic2006@gmail.com>
2024-07-21 22:03:54 +0300

    Select request change

--- Select_request.sql
+++ Select_request.sql
@@ -22,12 +22,9 @@ SELECT genre_id, COUNT(*) AS singer_count
 FROM singer_genre
 GROUP BY genre_id;
 
-SELECT COUNT(*) AS track_count 
-FROM tracks 
-WHERE album_id IN (
-	SELECT album_id
-	FROM albums
-	WHERE album_release BETWEEN '2019-01-01' AND '2020-12-31');
+SELECT count(*) FROM tracks t
+LEFT JOIN albums a ON a.album_id = t.album_id
+WHERE album_release BETWEEN '2019-01-01' AND '2020-12-31';
 
 SELECT album_id, AVG(duration) AS average_duration 
 FROM tracks
@@ -41,8 +38,13 @@ WHERE s.singer_id NOT IN (
     JOIN albums a ON sa.album_id = a.album_id
     WHERE a.album_release BETWEEN '2020-01-01' AND '2020-12-31');
     
-SELECT c.title
-FROM compilations c
-JOIN tracks t ON c.compilation_id = t.track_id
-JOIN singers p ON t.track_id = p.singer_id
-WHERE p.name = 'SkillGuy';
+SELECT s.name AS singer_name, c.title AS compilation_title
+FROM singers s
+JOIN singer_genre g ON s.singer_id = g.singer_id
+JOIN singer_album sa ON s.singer_id = sa.singer_id
+JOIN albums a ON sa.album_id = a.album_id
+JOIN tracks t ON a.album_id = t.album_id
+JOIN compilation_tracks ct ON t.track_id = ct.track_id
+JOIN compilations c ON ct.compilation_id = c.compilation_id
+WHERE s.name = 'Lana Del Rey'
+GROUP BY singer_name, compilation_title;
